c72adf78100d95ef2ef1e518121006d76075285c
Paul <itsdizzy40@gmail.com>
2025-11-29 23:54:46 +0100

    chore: use production branch of VU

--- Run Server.bat
+++ Run Server.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d %LOCALAPPDATA%\VeniceUnleashed\client
 :main
-vu.exe -server -console -headless -dedicated -debuglog -highResTerrain -skipChecksum -updatebranch dev
+vu.exe -server -console -headless -dedicated -debuglog -highResTerrain -skipChecksum -updatebranch prod
 goto :main
